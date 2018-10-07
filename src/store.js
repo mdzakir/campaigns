0ab2882520ed7b66fa67aa9de0c1410fdc32e535
ZakirMuhammad <muhammad.zakir@axisrooms.com>
2018-10-08 00:13:58 +0530

    commented devtools

--- src/store.js
+++ src/store.js
@@ -1,26 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
-import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
-import {createLogger} from 'redux-logger';
 
-/* const initialState = {};
-const middleware = [thunk]; */
+const initialState = {};
+const middleware = [thunk];
 
-const logger = createLogger({
-  collapsed: true,
-  diff: true
-});
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware)
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
-const  configureStore = function (initialState) {
-  return createStore(
-      rootReducer, 
-      initialState, 
-      composeWithDevTools(
-          /* logger must be the last middleware in chain to log actions */
-          applyMiddleware(thunk, logger)  
-      )
-  );
-}
-
-export default configureStore;
+export default store;
