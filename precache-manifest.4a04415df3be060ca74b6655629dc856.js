cebb98fbcdfe33fe8c83dc604d87d96a73057f27
ZakirMuhammad <muhammad.zakir@axisrooms.com>
2018-10-07 14:11:09 +0530

    Updates

---
+++ precache-manifest.4a04415df3be060ca74b6655629dc856.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "852f798a316479ee00b1",
+    "url": "/campaigns/static/js/runtime~main.852f798a.js"
+  },
+  {
+    "revision": "ad44692dbeb832338ead",
+    "url": "/campaigns/static/js/main.ad44692d.chunk.js"
+  },
+  {
+    "revision": "2b76cb7e9cc091574ca2",
+    "url": "/campaigns/static/js/1.2b76cb7e.chunk.js"
+  },
+  {
+    "revision": "ad44692dbeb832338ead",
+    "url": "/campaigns/static/css/main.d37f98b7.chunk.css"
+  },
+  {
+    "revision": "2b76cb7e9cc091574ca2",
+    "url": "/campaigns/static/css/1.9c54ae07.chunk.css"
+  },
+  {
+    "revision": "a518cb945c29020e0c3c438607137b6b",
+    "url": "/campaigns/index.html"
+  }
+];
