606770f5a07bfb2d94f274eba6ca95bc10ab4430
ZakirMuhammad <muhammad.zakir@axisrooms.com>
2018-10-08 00:08:34 +0530

    changed thunk

--- src/store.js
+++ src/store.js
@@ -4,21 +4,23 @@ import rootReducer from './reducers';
 import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 import {createLogger} from 'redux-logger';
 
-const initialState = {};
-const middleware = [thunk];
+/* const initialState = {};
+const middleware = [thunk]; */
 
 const logger = createLogger({
   collapsed: true,
   diff: true
 });
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(
-    applyMiddleware(...middleware),
-    (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-  )
-);
+const  configureStore = function (initialState) {
+  return createStore(
+      rootReducer, 
+      initialState, 
+      composeWithDevTools(
+          /* logger must be the last middleware in chain to log actions */
+          applyMiddleware(thunk, logger)  
+      )
+  );
+}
 
-export default store;
+export default configureStore;
