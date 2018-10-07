f48d3c3fcf7d2eb684559af5440a6614c8769854
ZakirMuhammad <muhammad.zakir@axisrooms.com>
2018-10-08 00:04:53 +0530

    compose to composewithdevtools

--- src/store.js
+++ src/store.js
@@ -1,16 +1,23 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
+import {createLogger} from 'redux-logger';
 
 const initialState = {};
 const middleware = [thunk];
 
+const logger = createLogger({
+  collapsed: true,
+  diff: true
+});
+
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
+  composeWithDevTools(
     applyMiddleware(...middleware),
-    (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) || null
+    (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
   )
 );
 
